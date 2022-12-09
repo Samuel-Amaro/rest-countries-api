0d137cb4d7e4a34ed7a35eb8efc29504af905787
Samuel Amaro <samuelamaro96746313@gmail.com>
2022-12-09 16:50:02 -0300

    add library react-router-dom with folder and newss files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import Root from './routers/Root';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+  }
+]);
+
+const root = ReactDOM.createRoot(document.querySelector(".Main"));
 root.render(
   <React.StrictMode>
     <App />
